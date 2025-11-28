f380efc45d9fa59fcb75145e9fc1eafb16b44389
jonnyell89 <jonnyell89@gmail.com>
2025-11-28 19:15:44 +0000

    @mui/x-data-grid installed, columns added to Cars component, Cars component return statement refactored to incorporate DataGrid component

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -1,8 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
+import { DataGrid } from "@mui/x-data-grid";
+import type { GridColDef } from "@mui/x-data-grid";
+
 import type { CarResponse } from "../types/CarResponse";
 import { getCars } from "../api/carAPI";
 
 function Cars() {
+
+    const columns: GridColDef[] = [
+
+        {field: "brand", headerName: "Brand", width: 200},
+        {field: "model", headerName: "Model", width: 200},
+        {field: "colour", headerName: "Colour", width: 200},
+        {field: "registrationNumber", headerName: "Reg No", width: 150},
+        {field: "modelYear", headerName: "Model Year", width: 150},
+        {field: "price", headerName: "Price", width: 150},
+    ];
 
     const { data, error, isSuccess } = useQuery<CarResponse[], Error>({
 
@@ -23,24 +36,36 @@ function Cars() {
     else {
 
         return (
-            
-            <table>
-                <tbody>
-                    {
-                        data.map((car: CarResponse) =>
-                        <tr key={car._links.self.href}>
-                            <td>{car.brand}</td>
-                            <td>{car.model}</td>
-                            <td>{car.colour}</td>
-                            <td>{car.registrationNumber}</td>
-                            <td>{car.modelYear}</td>
-                            <td>{car.price}</td>
-                        </tr>)
-                    }
-                </tbody>
-            </table>
+
+            <DataGrid
+                rows={data}
+                columns={columns}
+                getRowId={row => row._links.self.href}
+            />
         )
     }
+
+    // else {
+
+    //     return (
+            
+    //         <table>
+    //             <tbody>
+    //                 {
+    //                     data.map((car: CarResponse) =>
+    //                     <tr key={car._links.self.href}>
+    //                         <td>{car.brand}</td>
+    //                         <td>{car.model}</td>
+    //                         <td>{car.colour}</td>
+    //                         <td>{car.registrationNumber}</td>
+    //                         <td>{car.modelYear}</td>
+    //                         <td>{car.price}</td>
+    //                     </tr>)
+    //                 }
+    //             </tbody>
+    //         </table>
+    //     )
+    // }
 }
 
 export default Cars;
