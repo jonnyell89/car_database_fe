f81c6c735e238e53ca020f181c5d91b4126e5c5c
jonnyell89 <jonnyell89@gmail.com>
2025-11-28 16:39:05 +0000

    QueryClient component dependency injection, QueryClientProvider component added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
 
   return (
@@ -19,6 +23,7 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
     </Container>
   )
 }
