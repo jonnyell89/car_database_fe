acb42f3ccdb129b846d12f12b90b49cac3478730
jonnyell89 <jonnyell89@gmail.com>
2025-12-04 14:39:23 +0000

    App.test component added, testSetup.ts file added, vite.config.ts file modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    // setupFiles: "./src/testSetup.ts",
+    // css: false,
+    server: {
+      deps: {
+        inline: ["@mui/x-data-grid"],
+      },
+    },
+  },
 })
