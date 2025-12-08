b773afa5582c2bef69592f97d72f3ee98af97baa
jonnyell89 <jonnyell89@gmail.com>
2025-12-08 14:06:41 +0000

    'Authorization: token' added to the header of all carAPI functions

--- src/api/carAPI.ts
+++ src/api/carAPI.ts
@@ -5,10 +5,13 @@ import type { CarResponse } from "../types/CarResponse";
 
 export const addCar = async (car: Car): Promise<CarResponse> => {
 
+    const token = sessionStorage.getItem("jwt");
+
     const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/cars`, car, {
 
         headers: {
             "Content-Type": "application/json",
+            "Authorization": token,
         },
     });
 
@@ -17,17 +20,27 @@ export const addCar = async (car: Car): Promise<CarResponse> => {
 
 export const getCars = async (): Promise<CarResponse[]> => {
 
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`);
+    const token = sessionStorage.getItem("jwt");
+
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`, {
+        
+        headers: { 
+            "Authorization": token,
+        },
+    });
 
     return response.data._embedded.cars;
 }
 
 export const updateCar = async (carEntry: CarEntry): Promise<CarResponse> => {
 
+    const token = sessionStorage.getItem("jwt");
+
     const response = await axios.put(carEntry.url, carEntry.car, {
 
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": token,
         },
     });
     
@@ -36,7 +49,14 @@ export const updateCar = async (carEntry: CarEntry): Promise<CarResponse> => {
 
 export const deleteCar = async (link: string): Promise<CarResponse> => {
 
-    const response = await axios.delete(link);
+    const token = sessionStorage.getItem("jwt");
+
+    const response = await axios.delete(link, {
+
+        headers: {
+            "Authorization": token
+        }
+    });
 
     return response.data;
 }
